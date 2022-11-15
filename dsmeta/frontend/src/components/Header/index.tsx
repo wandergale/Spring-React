7ecf712903a0f4b8f4aadd5491a379bc0eadbcc6
Wanderson <wandersongamer2@gmail.com>
2022-11-15 18:49:32 -0300

    Other components

---
+++ dsmeta/frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/img/logo.svg"
+
+import "./styles.css"
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/wanderson.gale"> @wanderson.gale</a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
