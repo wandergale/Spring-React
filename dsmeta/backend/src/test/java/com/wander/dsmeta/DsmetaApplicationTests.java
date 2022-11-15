9e665f1416de3a1ec2bee87a4f2337c4be43f82b
Wanderson <wandersongamer2@gmail.com>
2022-11-15 15:57:50 -0300

    backend created

---
+++ dsmeta/backend/src/test/java/com/wander/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
